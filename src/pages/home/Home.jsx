bdde70f1e8f226ba28c4dc2efeb1c0ed3890f433
RukshanNuwan <shanwijendra.dev@gmail.com>
2024-06-27 23:08:39 +0530

    dump

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,21 +1,21 @@
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'remixicon/fonts/remixicon.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "remixicon/fonts/remixicon.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-import Header from '../../components/header/Header';
-import SideBar from '../../components/sideBar/SideBar';
-import Main from '../../components/main/Main';
-import Footer from '../../components/footer/Footer';
-import BackToTop from '../../components/backToTop/BackToTop';
+import Header from "../../components/header/Header";
+import SideBar from "../../components/sideBar/SideBar";
+import Main from "../../components/main/Main";
+import Footer from "../../components/footer/Footer";
+import BackToTop from "../../components/backToTop/BackToTop";
 
 const Home = () => {
   return (
     <>
       <Header />
       <SideBar />
-      <Main />
+      {/* <Main /> */}
       <Footer />
       <BackToTop />
     </>
